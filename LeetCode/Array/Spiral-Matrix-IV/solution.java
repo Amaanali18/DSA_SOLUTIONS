e902c423fb2f0671ed964d70eff65e68196b7c41
Amaanali18 <amaanaliparchigar0918@gmail.com>
2026-08-11 17:00:23 +0530

    feat(leetcode): Add Spiral Matrix IV (#spiral-matrix-iv)

---
+++ LeetCode/Array/Spiral-Matrix-IV/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] ans = new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(ans[i],-1);
+        }
+        int minr = 0;
+        int maxr = m-1;
+        int minc = 0;
+        int maxc = n-1;
+        while(head!=null){
+            for(int i=minc;i<=maxc && head!=null;i++){
+                ans[minr][i]=head.val;
+                head=head.next;
+            }
+            minr++;
+            for(int i=minr;i<=maxr && head!=null;i++){
+                ans[i][maxc]=head.val;
+                head=head.next;
+            }
+            maxc--;
+            for(int i=maxc;i>=minc && head!=null;i--){
+                ans[maxr][i]=head.val;
+                head=head.next;
+            }
+            maxr--;
+            for(int i=maxr;i>=minr && head!=null;i--){
+                ans[i][minc]=head.val;
+                head=head.next;
+            }
+            minc++;
+        }
+        return ans;
+    }
+}
